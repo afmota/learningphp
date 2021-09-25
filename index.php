05838eb87fedbb22d0f890a18b5e47f465752cf0
Agnaldo Mota <af_mota@hotmail.com>
2021-09-25 16:14:19 -0300

    Adicionando validação de campos do formulário

--- index.php
+++ index.php
@@ -1,39 +1,23 @@
-<?php
-  $categorias = [];
-  $categorias[] = 'infantil';
-  $categorias[] = 'adolescente';
-  $categorias[] = 'adulto';
-  $categorias[] = 'idoso';
-  //print_r($categorias);
-  
-  $nome = 'Eduardo';
-  $idade = 8;
-  
-  //var_dump($nome);
-  //var_dump($idade);
-  
-  if ($idade >= 6 && $idade <= 12)
-  {
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-      if ($categorias[$i] == 'infantil')
-        echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
-    }
-  }
-  else if ($idade >= 13 && $idade <= 18)
-  {
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-      if ($categorias[$i] == 'adolescente')
-        echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
-    }
-  }
-  else
-  {
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-      if ($categorias[$i] == 'adulto')
-        echo "O nadador " .$nome. " compete na categoria " .$categorias[$i];
-    }
-  }
-  phpinfo();
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Formulário de Inscrição</title>
+	<meta name="author" content="">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<p>FORMULÁRIO DE INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+	<p>Seu nome: <input type="text" name="nome" /></p>
+	<p>Sua idade: <input type="text" name="idade" /></p>
+	<p><input type="submit" value="Enviar dados do competidor"/></p>
+</form>
+
+</body>
+
+</html>
