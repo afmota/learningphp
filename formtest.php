7712107b84016d3edf0a18d24113a1634fcad4a8
Agnaldo Mota <af_mota@hotmail.com>
2021-10-19 16:50:07 -0300

    processando dados de um campo de formulário

--- formtest.php
+++ formtest.php
@@ -1,13 +1,20 @@
 <?php //formtest.php
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+} else {
+    $name = "(Não informado)";
+}
+
 echo <<<_END
 <html>
     <head>
         <title>Formulário Teste</title>
     </head>
     <body>
+        Seu nome é: $name<br>
         <form method="post" action="formtest.php">
             Qual o seu nome?
-            <input type="text" name="nome">
+            <input type="text" name="name">
             <input type="submit">
         </form>
     </body>
